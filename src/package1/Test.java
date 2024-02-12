6f7769adefd2593b9b29ad567f634c2f5ba66f89
Rukshan Perera <rukshanperera122@gmail.com>
2024-02-12 16:52:49 +0530

    first commit

---
+++ src/package1/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package package1;
+
+/**
+ *
+ * @author User
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
